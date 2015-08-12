ccb2bd3defd3fac68426474d2c9aba5b774974fb
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-08-12 10:47:33 +0200

    Dice chance evaluator tested

---
+++ DrdPlus/Tests/Tables/DiceChanceEvaluatorTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace DrdPlus\Tests\Tables;
+
+use Drd\DiceRoll\Templates\Rolls\Roll1d6;
+use DrdPlus\Tables\DiceChanceEvaluator;
+
+class DiceChanceEvaluatorTest extends TestWithMockery
+{
+
+    /**
+     * @test
+     */
+    public function I_can_evaluate_chance_by_dice_roll()
+    {
+        /** @var \Mockery\MockInterface|Roll1d6 $roll */
+        $roll = $this->mockery(Roll1d6::class);
+        $evaluator = new DiceChanceEvaluator($roll);
+        $roll->shouldReceive('roll')
+            ->twice()
+            ->andReturnValues([$higherRoll = 321, $lowerRoll = 123]);
+        $this->assertSame(1, $evaluator->evaluate($higherRoll - 1));
+        $this->assertSame(0, $evaluator->evaluate($lowerRoll + 1));
+    }
+}
