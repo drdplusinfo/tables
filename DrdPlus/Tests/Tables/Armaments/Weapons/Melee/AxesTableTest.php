99a1aaa68fad6dca1074c569d60d2a0e64aa0f80
jaroslavtyc <mail@jaroslavtyc.com>
2016-04-20 07:06:00 +0200

    Melee weapon tests generalized a bit

--- DrdPlus/Tests/Tables/Armaments/Weapons/Melee/AxesTableTest.php
+++ DrdPlus/Tests/Tables/Armaments/Weapons/Melee/AxesTableTest.php
@@ -3,7 +3,7 @@ namespace DrdPlus\Tests\Tables\Armaments\Weapons\Melee;
 
 use DrdPlus\Codes\WeaponCodes;
 use DrdPlus\Codes\WoundTypeCodes;
-use DrdPlus\Tables\Armaments\Weapons\Melee\AxesTable;
+use DrdPlus\Tables\Armaments\Weapons\Melee\Partials\AbstractMeleeWeaponsTable;
 use DrdPlus\Tests\Tables\Armaments\Weapons\Melee\Partials\AbstractMeleeWeaponsTableTest;
 
 class AxesTableTest extends AbstractMeleeWeaponsTableTest
@@ -12,37 +12,37 @@ class AxesTableTest extends AbstractMeleeWeaponsTableTest
     public function provideWeaponAndNameWithValue()
     {
         return [
-            [WeaponCodes::LIGHT_AXE, AxesTable::REQUIRED_STRENGTH_HEADER, 3],
-            [WeaponCodes::LIGHT_AXE, AxesTable::LENGTH_HEADER, 1],
-            [WeaponCodes::LIGHT_AXE, AxesTable::OFFENSIVENESS_HEADER, 3],
-            [WeaponCodes::LIGHT_AXE, AxesTable::WOUNDS_HEADER, 3],
-            [WeaponCodes::LIGHT_AXE, AxesTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::CUT],
-            [WeaponCodes::LIGHT_AXE, AxesTable::COVER_HEADER, 2],
-            [WeaponCodes::LIGHT_AXE, AxesTable::WEIGHT_HEADER, 1.0],
+            [WeaponCodes::LIGHT_AXE, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, 3],
+            [WeaponCodes::LIGHT_AXE, AbstractMeleeWeaponsTable::LENGTH_HEADER, 1],
+            [WeaponCodes::LIGHT_AXE, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 3],
+            [WeaponCodes::LIGHT_AXE, AbstractMeleeWeaponsTable::WOUNDS_HEADER, 3],
+            [WeaponCodes::LIGHT_AXE, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::CUT],
+            [WeaponCodes::LIGHT_AXE, AbstractMeleeWeaponsTable::COVER_HEADER, 2],
+            [WeaponCodes::LIGHT_AXE, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 1.0],
 
-            [WeaponCodes::AXE, AxesTable::REQUIRED_STRENGTH_HEADER, 6],
-            [WeaponCodes::AXE, AxesTable::LENGTH_HEADER, 2],
-            [WeaponCodes::AXE, AxesTable::OFFENSIVENESS_HEADER, 3],
-            [WeaponCodes::AXE, AxesTable::WOUNDS_HEADER, 5],
-            [WeaponCodes::AXE, AxesTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::CUT],
-            [WeaponCodes::AXE, AxesTable::COVER_HEADER, 2],
-            [WeaponCodes::AXE, AxesTable::WEIGHT_HEADER, 2.0],
+            [WeaponCodes::AXE, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, 6],
+            [WeaponCodes::AXE, AbstractMeleeWeaponsTable::LENGTH_HEADER, 2],
+            [WeaponCodes::AXE, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 3],
+            [WeaponCodes::AXE, AbstractMeleeWeaponsTable::WOUNDS_HEADER, 5],
+            [WeaponCodes::AXE, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::CUT],
+            [WeaponCodes::AXE, AbstractMeleeWeaponsTable::COVER_HEADER, 2],
+            [WeaponCodes::AXE, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 2.0],
 
-            [WeaponCodes::WAR_AXE, AxesTable::REQUIRED_STRENGTH_HEADER, 9],
-            [WeaponCodes::WAR_AXE, AxesTable::LENGTH_HEADER, 3],
-            [WeaponCodes::WAR_AXE, AxesTable::OFFENSIVENESS_HEADER, 3],
-            [WeaponCodes::WAR_AXE, AxesTable::WOUNDS_HEADER, 7],
-            [WeaponCodes::WAR_AXE, AxesTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::CUT],
-            [WeaponCodes::WAR_AXE, AxesTable::COVER_HEADER, 3],
-            [WeaponCodes::WAR_AXE, AxesTable::WEIGHT_HEADER, 2.5],
+            [WeaponCodes::WAR_AXE, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, 9],
+            [WeaponCodes::WAR_AXE, AbstractMeleeWeaponsTable::LENGTH_HEADER, 3],
+            [WeaponCodes::WAR_AXE, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 3],
+            [WeaponCodes::WAR_AXE, AbstractMeleeWeaponsTable::WOUNDS_HEADER, 7],
+            [WeaponCodes::WAR_AXE, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::CUT],
+            [WeaponCodes::WAR_AXE, AbstractMeleeWeaponsTable::COVER_HEADER, 3],
+            [WeaponCodes::WAR_AXE, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 2.5],
 
-            [WeaponCodes::TWO_HANDED_AXE, AxesTable::REQUIRED_STRENGTH_HEADER, 12],
-            [WeaponCodes::TWO_HANDED_AXE, AxesTable::LENGTH_HEADER, 3],
-            [WeaponCodes::TWO_HANDED_AXE, AxesTable::OFFENSIVENESS_HEADER, 4],
-            [WeaponCodes::TWO_HANDED_AXE, AxesTable::WOUNDS_HEADER, 10],
-            [WeaponCodes::TWO_HANDED_AXE, AxesTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::CUT],
-            [WeaponCodes::TWO_HANDED_AXE, AxesTable::COVER_HEADER, 3],
-            [WeaponCodes::TWO_HANDED_AXE, AxesTable::WEIGHT_HEADER, 3.0],
+            [WeaponCodes::TWO_HANDED_AXE, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, 12],
+            [WeaponCodes::TWO_HANDED_AXE, AbstractMeleeWeaponsTable::LENGTH_HEADER, 3],
+            [WeaponCodes::TWO_HANDED_AXE, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 4],
+            [WeaponCodes::TWO_HANDED_AXE, AbstractMeleeWeaponsTable::WOUNDS_HEADER, 10],
+            [WeaponCodes::TWO_HANDED_AXE, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::CUT],
+            [WeaponCodes::TWO_HANDED_AXE, AbstractMeleeWeaponsTable::COVER_HEADER, 3],
+            [WeaponCodes::TWO_HANDED_AXE, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 3.0],
         ];
     }
 
--- DrdPlus/Tests/Tables/Armaments/Weapons/Melee/KnifesAndDaggersTableTest.php
+++ DrdPlus/Tests/Tables/Armaments/Weapons/Melee/KnifesAndDaggersTableTest.php
@@ -3,7 +3,7 @@ namespace DrdPlus\Tests\Tables\Armaments\Weapons\Melee;
 
 use DrdPlus\Codes\WeaponCodes;
 use DrdPlus\Codes\WoundTypeCodes;
-use DrdPlus\Tables\Armaments\Weapons\Melee\KnifesAndDaggersTable;
+use DrdPlus\Tables\Armaments\Weapons\Melee\Partials\AbstractMeleeWeaponsTable;
 use DrdPlus\Tests\Tables\Armaments\Weapons\Melee\Partials\AbstractMeleeWeaponsTableTest;
 
 class KnifesAndDaggersTableTest extends AbstractMeleeWeaponsTableTest
@@ -11,45 +11,45 @@ class KnifesAndDaggersTableTest extends AbstractMeleeWeaponsTableTest
     public function provideWeaponAndNameWithValue()
     {
         return [
-            [WeaponCodes::KNIFE, KnifesAndDaggersTable::REQUIRED_STRENGTH_HEADER, -3],
-            [WeaponCodes::KNIFE, KnifesAndDaggersTable::LENGTH_HEADER, 0],
-            [WeaponCodes::KNIFE, KnifesAndDaggersTable::OFFENSIVENESS_HEADER, 0],
-            [WeaponCodes::KNIFE, KnifesAndDaggersTable::WOUNDS_HEADER, -2],
-            [WeaponCodes::KNIFE, KnifesAndDaggersTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
-            [WeaponCodes::KNIFE, KnifesAndDaggersTable::COVER_HEADER, 1],
-            [WeaponCodes::KNIFE, KnifesAndDaggersTable::WEIGHT_HEADER, 0.2],
+            [WeaponCodes::KNIFE, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, -3],
+            [WeaponCodes::KNIFE, AbstractMeleeWeaponsTable::LENGTH_HEADER, 0],
+            [WeaponCodes::KNIFE, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 0],
+            [WeaponCodes::KNIFE, AbstractMeleeWeaponsTable::WOUNDS_HEADER, -2],
+            [WeaponCodes::KNIFE, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
+            [WeaponCodes::KNIFE, AbstractMeleeWeaponsTable::COVER_HEADER, 1],
+            [WeaponCodes::KNIFE, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 0.2],
 
-            [WeaponCodes::DAGGER, KnifesAndDaggersTable::REQUIRED_STRENGTH_HEADER, -1],
-            [WeaponCodes::DAGGER, KnifesAndDaggersTable::LENGTH_HEADER, 0],
-            [WeaponCodes::DAGGER, KnifesAndDaggersTable::OFFENSIVENESS_HEADER, 1],
-            [WeaponCodes::DAGGER, KnifesAndDaggersTable::WOUNDS_HEADER, 1],
-            [WeaponCodes::DAGGER, KnifesAndDaggersTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
-            [WeaponCodes::DAGGER, KnifesAndDaggersTable::COVER_HEADER, 2],
-            [WeaponCodes::DAGGER, KnifesAndDaggersTable::WEIGHT_HEADER, 0.2],
+            [WeaponCodes::DAGGER, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, -1],
+            [WeaponCodes::DAGGER, AbstractMeleeWeaponsTable::LENGTH_HEADER, 0],
+            [WeaponCodes::DAGGER, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 1],
+            [WeaponCodes::DAGGER, AbstractMeleeWeaponsTable::WOUNDS_HEADER, 1],
+            [WeaponCodes::DAGGER, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
+            [WeaponCodes::DAGGER, AbstractMeleeWeaponsTable::COVER_HEADER, 2],
+            [WeaponCodes::DAGGER, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 0.2],
 
-            [WeaponCodes::STABBING_DAGGER, KnifesAndDaggersTable::REQUIRED_STRENGTH_HEADER, -1],
-            [WeaponCodes::STABBING_DAGGER, KnifesAndDaggersTable::LENGTH_HEADER, 0],
-            [WeaponCodes::STABBING_DAGGER, KnifesAndDaggersTable::OFFENSIVENESS_HEADER, 2],
-            [WeaponCodes::STABBING_DAGGER, KnifesAndDaggersTable::WOUNDS_HEADER, 0],
-            [WeaponCodes::STABBING_DAGGER, KnifesAndDaggersTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
-            [WeaponCodes::STABBING_DAGGER, KnifesAndDaggersTable::COVER_HEADER, 1],
-            [WeaponCodes::STABBING_DAGGER, KnifesAndDaggersTable::WEIGHT_HEADER, 0.2],
+            [WeaponCodes::STABBING_DAGGER, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, -1],
+            [WeaponCodes::STABBING_DAGGER, AbstractMeleeWeaponsTable::LENGTH_HEADER, 0],
+            [WeaponCodes::STABBING_DAGGER, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 2],
+            [WeaponCodes::STABBING_DAGGER, AbstractMeleeWeaponsTable::WOUNDS_HEADER, 0],
+            [WeaponCodes::STABBING_DAGGER, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
+            [WeaponCodes::STABBING_DAGGER, AbstractMeleeWeaponsTable::COVER_HEADER, 1],
+            [WeaponCodes::STABBING_DAGGER, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 0.2],
 
-            [WeaponCodes::LONG_KNIFE, KnifesAndDaggersTable::REQUIRED_STRENGTH_HEADER, -2],
-            [WeaponCodes::LONG_KNIFE, KnifesAndDaggersTable::LENGTH_HEADER, 1],
-            [WeaponCodes::LONG_KNIFE, KnifesAndDaggersTable::OFFENSIVENESS_HEADER, 1],
-            [WeaponCodes::LONG_KNIFE, KnifesAndDaggersTable::WOUNDS_HEADER, -1],
-            [WeaponCodes::LONG_KNIFE, KnifesAndDaggersTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
-            [WeaponCodes::LONG_KNIFE, KnifesAndDaggersTable::COVER_HEADER, 1],
-            [WeaponCodes::LONG_KNIFE, KnifesAndDaggersTable::WEIGHT_HEADER, 0.2],
+            [WeaponCodes::LONG_KNIFE, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, -2],
+            [WeaponCodes::LONG_KNIFE, AbstractMeleeWeaponsTable::LENGTH_HEADER, 1],
+            [WeaponCodes::LONG_KNIFE, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 1],
+            [WeaponCodes::LONG_KNIFE, AbstractMeleeWeaponsTable::WOUNDS_HEADER, -1],
+            [WeaponCodes::LONG_KNIFE, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
+            [WeaponCodes::LONG_KNIFE, AbstractMeleeWeaponsTable::COVER_HEADER, 1],
+            [WeaponCodes::LONG_KNIFE, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 0.2],
 
-            [WeaponCodes::LONG_DAGGER, KnifesAndDaggersTable::REQUIRED_STRENGTH_HEADER, 1],
-            [WeaponCodes::LONG_DAGGER, KnifesAndDaggersTable::LENGTH_HEADER, 1],
-            [WeaponCodes::LONG_DAGGER, KnifesAndDaggersTable::OFFENSIVENESS_HEADER, 1],
-            [WeaponCodes::LONG_DAGGER, KnifesAndDaggersTable::WOUNDS_HEADER, 2],
-            [WeaponCodes::LONG_DAGGER, KnifesAndDaggersTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
-            [WeaponCodes::LONG_DAGGER, KnifesAndDaggersTable::COVER_HEADER, 2],
-            [WeaponCodes::LONG_DAGGER, KnifesAndDaggersTable::WEIGHT_HEADER, 0.3],
+            [WeaponCodes::LONG_DAGGER, AbstractMeleeWeaponsTable::REQUIRED_STRENGTH_HEADER, 1],
+            [WeaponCodes::LONG_DAGGER, AbstractMeleeWeaponsTable::LENGTH_HEADER, 1],
+            [WeaponCodes::LONG_DAGGER, AbstractMeleeWeaponsTable::OFFENSIVENESS_HEADER, 1],
+            [WeaponCodes::LONG_DAGGER, AbstractMeleeWeaponsTable::WOUNDS_HEADER, 2],
+            [WeaponCodes::LONG_DAGGER, AbstractMeleeWeaponsTable::WOUNDS_TYPE_HEADER, WoundTypeCodes::STAB],
+            [WeaponCodes::LONG_DAGGER, AbstractMeleeWeaponsTable::COVER_HEADER, 2],
+            [WeaponCodes::LONG_DAGGER, AbstractMeleeWeaponsTable::WEIGHT_HEADER, 0.3],
         ];
     }
 
