e21ff0eea68052ad05dfa1fb282d9e7bea1aaab6
jaroslavtyc <mail@jaroslavtyc.com>
2016-04-27 06:22:17 +0200

    fix: An interface return value annotation is less dependent

--- DrdPlus/Tables/Measurements/MeasurementWithBonus.php
+++ DrdPlus/Tables/Measurements/MeasurementWithBonus.php
@@ -1,12 +1,10 @@
 <?php
 namespace DrdPlus\Tables\Measurements;
 
-use DrdPlus\Tables\Measurements\Partials\AbstractBonus;
-
 interface MeasurementWithBonus extends Measurement
 {
     /**
-     * @return AbstractBonus
+     * @return Bonus
      */
     public function getBonus();
 
