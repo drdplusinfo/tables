fd73afe1c21b126b99863fedf138c2988fa5d2b4
jaroslavtyc <mail@jaroslavtyc.com>
2016-09-09 14:01:54 +0200

    Combat actions compatibility table

---
+++ DrdPlus/Tables/Actions/CombatActionsCompatibilityTable.php
@@ -0,0 +1,93 @@
+<?php
+namespace DrdPlus\Tables\Actions;
+
+use DrdPlus\Codes\CombatActions\CombatActionCode;
+use DrdPlus\Codes\CombatActions\MeleeCombatActionCode;
+use DrdPlus\Codes\CombatActions\RangedCombatActionCode;
+use DrdPlus\Tables\Partials\AbstractFileTable;
+
+class CombatActionsCompatibilityTable extends AbstractFileTable
+{
+    /**
+     * @return string
+     */
+    protected function getDataFileName()
+    {
+        return __DIR__ . '/data/combat_actions_compatibility.csv';
+    }
+
+    protected function getExpectedDataHeaderNamesToTypes()
+    {
+        return [
+            CombatActionCode::MOVE => self::BOOLEAN,
+            CombatActionCode::RUN => self::BOOLEAN,
+            CombatActionCode::STANDARD_ATTACK => self::BOOLEAN,
+            CombatActionCode::SWAP_WEAPONS => self::BOOLEAN,
+            CombatActionCode::CONCENTRATION_ON_DEFENSE => self::BOOLEAN,
+            CombatActionCode::PUT_OUT_EASILY_ACCESSIBLE_ITEM => self::BOOLEAN,
+            CombatActionCode::PUT_OUT_HARDLY_ACCESSIBLE_ITEM => self::BOOLEAN,
+            CombatActionCode::LAYING => self::BOOLEAN,
+            CombatActionCode::SITTING_OR_ON_KNEELS => self::BOOLEAN,
+            CombatActionCode::GETTING_UP => self::BOOLEAN,
+            CombatActionCode::PUTTING_ON_ARMOR => self::BOOLEAN,
+            CombatActionCode::ATTACK_FROM_BEHIND => self::BOOLEAN,
+            CombatActionCode::BLINDFOLD_FIGHT => self::BOOLEAN,
+            CombatActionCode::FIGHT_IN_REDUCED_VISIBILITY => self::BOOLEAN,
+            CombatActionCode::ATTACK_ON_DISABLED_OPPONENT => self::BOOLEAN,
+            MeleeCombatActionCode::HEADLESS_ATTACK => self::BOOLEAN,
+            MeleeCombatActionCode::COVER_OF_ALLY => self::BOOLEAN,
+            MeleeCombatActionCode::FIGHT_WITH_TWO_WEAPONS => self::BOOLEAN,
+            MeleeCombatActionCode::FLAT_ATTACK => self::BOOLEAN,
+            MeleeCombatActionCode::PRESSURE => self::BOOLEAN,
+            MeleeCombatActionCode::RETREAT => self::BOOLEAN,
+            MeleeCombatActionCode::HANDOVER_ITEM => self::BOOLEAN,
+            RangedCombatActionCode::AIMED_SHOT => self::BOOLEAN,
+        ];
+    }
+
+    const ACTION = 'action';
+
+    protected function getRowsHeader()
+    {
+        return [self::ACTION];
+    }
+
+    /**
+     * @param string $someAction
+     * @param string $anotherAction
+     * @return bool
+     * @throws \DrdPlus\Tables\Partials\Exceptions\RequiredRowNotFound
+     * @throws \DrdPlus\Tables\Partials\Exceptions\RequiredColumnNotFound
+     * @throws \Granam\Scalar\Tools\Exceptions\WrongParameterType
+     */
+    public function canCombineTwoActions($someAction, $anotherAction)
+    {
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
+        return $this->getValue($someAction, $anotherAction);
+    }
+
+    /**
+     * @param array|string[] $actions
+     * @return bool
+     * @throws \DrdPlus\Tables\Partials\Exceptions\RequiredRowNotFound
+     * @throws \DrdPlus\Tables\Partials\Exceptions\RequiredColumnNotFound
+     * @throws \Granam\Scalar\Tools\Exceptions\WrongParameterType
+     */
+    public function canCombineActions(array $actions)
+    {
+        if (count($actions) < 2) {
+            return true;
+        }
+        foreach ($actions as $someAction) {
+            foreach ($actions as $anotherAction) {
+                /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
+                if (!$this->getValue($someAction, $anotherAction)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+}
---
+++ DrdPlus/Tests/Tables/Actions/CombatActionsCompatibilityTableTest.php
@@ -0,0 +1,110 @@
+<?php
+namespace DrdPlus\Tables\Actions;
+
+use DrdPlus\Codes\CombatActions\CombatActionCode;
+use DrdPlus\Codes\CombatActions\MeleeCombatActionCode;
+use DrdPlus\Codes\CombatActions\RangedCombatActionCode;
+use DrdPlus\Tests\Tables\TableTestInterface;
+use Granam\Tests\Tools\TestWithMockery;
+
+class CombatActionsCompatibilityTableTest extends TestWithMockery implements TableTestInterface
+{
+    /**
+     * @test
+     */
+    public function I_can_get_header()
+    {
+        self::assertSame(
+            [
+                [
+                    'action',
+                    'move',
+                    'run',
+                    'standard_attack',
+                    'swap_weapons',
+                    'concentration_on_defense',
+                    'put_out_easily_accessible_item',
+                    'put_out_hardly_accessible_item',
+                    'laying',
+                    'sitting_or_on_kneels',
+                    'getting_up',
+                    'putting_on_armor',
+                    'attack_from_behind',
+                    'blindfold_fight',
+                    'fight_in_reduced_visibility',
+                    'attack_on_disabled_opponent',
+                    'headless_attack',
+                    'cover_of_ally',
+                    'fight_with_two_weapons',
+                    'flat_attack',
+                    'pressure',
+                    'retreat',
+                    'handover_item',
+                    'aimed_shot',
+                ],
+            ],
+            (new CombatActionsCompatibilityTable())->getHeader()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function Combinations_are_same_from_both_sides()
+    {
+        $codes = CombatActionCode::getCombatActionCodes();
+        $codes = array_merge($codes, MeleeCombatActionCode::getMeleeOnlyCombatActionCodes());
+        $codes = array_merge($codes, RangedCombatActionCode::getRangedOnlyCombatActionCodes());
+        $combatActionsCompatibilityTable = new CombatActionsCompatibilityTable();
+        foreach ($codes as $someCode) {
+            foreach ($codes as $anotherCode) {
+                self::assertSame(
+                    $combatActionsCompatibilityTable->getValue($someCode, $anotherCode),
+                    $combatActionsCompatibilityTable->getValue($anotherCode, $someCode),
+                    "'{$someCode}' and '{$anotherCode}' do not match from both sides"
+                );
+            }
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_find_out_easily_if_two_actions_can_be_combined()
+    {
+        $combatActionsCompatibilityTable = new CombatActionsCompatibilityTable();
+        self::assertTrue($combatActionsCompatibilityTable->canCombineTwoActions(
+            CombatActionCode::ATTACK_FROM_BEHIND,
+            CombatActionCode::MOVE
+        ));
+        self::assertFalse($combatActionsCompatibilityTable->canCombineTwoActions(
+            CombatActionCode::GETTING_UP,
+            CombatActionCode::LAYING
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_find_out_easily_if_any_actions_can_be_combined()
+    {
+        $combatActionsCompatibilityTable = new CombatActionsCompatibilityTable();
+        self::assertTrue($combatActionsCompatibilityTable->canCombineActions([MeleeCombatActionCode::COVER_OF_ALLY]));
+        self::assertTrue($combatActionsCompatibilityTable->canCombineActions([
+            CombatActionCode::ATTACK_FROM_BEHIND,
+            CombatActionCode::MOVE,
+            CombatActionCode::FIGHT_IN_REDUCED_VISIBILITY,
+            CombatActionCode::PUT_OUT_EASILY_ACCESSIBLE_ITEM,
+            MeleeCombatActionCode::RETREAT,
+        ]));
+        self::assertFalse($combatActionsCompatibilityTable->canCombineActions([
+            CombatActionCode::ATTACK_FROM_BEHIND,
+            CombatActionCode::MOVE,
+            CombatActionCode::FIGHT_IN_REDUCED_VISIBILITY,
+            CombatActionCode::PUT_OUT_EASILY_ACCESSIBLE_ITEM,
+            MeleeCombatActionCode::RETREAT,
+            RangedCombatActionCode::AIMED_SHOT,
+        ]));
+    }
+
+}
