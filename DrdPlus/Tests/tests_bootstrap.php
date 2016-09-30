8392d0c11e13bbdbc3c7f9dd630676d7ff19a0bd
Jaroslav Týc <mail@jaroslavtyc.com>
2016-09-30 16:56:59 +0200

    PHP native functions are called in type strict mode if PHP 7+

--- DrdPlus/Tests/tests_bootstrap.php
+++ DrdPlus/Tests/tests_bootstrap.php
@@ -1,6 +1,9 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
-error_reporting(-1);
-ini_set('display_errors', true);
+declare(strict_types = 1); // on PHP 7+ are standard PHP methods strict to types of given parameters
 
-ini_set('xdebug.max_nesting_level', 100);
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+error_reporting(-1);
+ini_set('display_errors', '1');
+
+ini_set('xdebug.max_nesting_level', '100');
