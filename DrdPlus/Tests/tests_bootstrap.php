02b7a23993a4ad2aa4ee930d990df8715581e550
Jaroslav Týc <mail@jaroslavtyc.com>
2016-11-02 11:35:08 +0100

    Asserts in tests are active for Travis CI

--- DrdPlus/Tests/tests_bootstrap.php
+++ DrdPlus/Tests/tests_bootstrap.php
@@ -13,4 +13,5 @@ ini_set('xdebug.max_nesting_level', '100');
 if (!ini_get('zend.assertions')) {
     trigger_error("Assert() wil not be evaluated. Please set in on in php.ini for testing\n", E_USER_WARNING);
 }
+ini_set('assert.active', '1');
 ini_set('assert.exception', '1'); // throw an exception instead of an error
