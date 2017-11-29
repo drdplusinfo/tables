8ec412d2a119162a33b34d51241649bc6de4dad3
Jaroslav Týc <mail@jaroslavtyc.com>
2017-11-29 08:14:20 +0100

    Tested creation of fatigue with a time object as a value

--- DrdPlus/Tests/Tables/Measurements/Fatigue/FatigueTest.php
+++ DrdPlus/Tests/Tables/Measurements/Fatigue/FatigueTest.php
@@ -6,7 +6,9 @@ namespace DrdPlus\Tests\Tables\Measurements\Fatigue;
 use DrdPlus\Tables\Measurements\Fatigue\Fatigue;
 use DrdPlus\Tables\Measurements\Fatigue\FatigueTable;
 use DrdPlus\Tables\Measurements\Measurement;
+use DrdPlus\Tables\Measurements\Time\Time;
 use DrdPlus\Tables\Table;
+use DrdPlus\Tables\Tables;
 use DrdPlus\Tests\Tables\Measurements\AbstractTestOfMeasurement;
 
 class FatigueTest extends AbstractTestOfMeasurement
@@ -26,5 +28,14 @@ class FatigueTest extends AbstractTestOfMeasurement
 
         /** @var $table FatigueTable */
         return new Fatigue($amount, $table);
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_create_it_with_time_object_as_value()
+    {
+        $fatigue = new Fatigue(new Time(123, Time::MINUTE, Tables::getIt()->getTimeTable()), Tables::getIt()->getFatigueTable());
+        self::assertSame(123, $fatigue->getValue());
     }
 }
