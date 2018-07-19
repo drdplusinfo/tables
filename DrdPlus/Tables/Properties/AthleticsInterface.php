f2ece09233501d1cd5fa6b102d1c9f901e24137a
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-19 10:09:13 +0200

    Removed useless requirement from athletics

--- DrdPlus/Tables/Properties/AthleticsInterface.php
+++ DrdPlus/Tables/Properties/AthleticsInterface.php
@@ -3,13 +3,12 @@ declare(strict_types=1);
 /** be strict for parameter types, https://www.quora.com/Are-strict_types-in-PHP-7-not-a-bad-idea */
 namespace DrdPlus\Tables\Properties;
 
-use Granam\Integer\IntegerInterface;
 use Granam\Integer\PositiveInteger;
 
 /**
  * Just an interface to cover requirements. It is not implemented in this library.
  */
-interface AthleticsInterface extends IntegerInterface
+interface AthleticsInterface
 {
     /**
      * @return PositiveInteger
