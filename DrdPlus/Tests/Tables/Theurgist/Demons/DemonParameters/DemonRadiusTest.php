a55719e8c1556b87f76d3af1bf64d8db33b7cfa6
Jaroslav Týc <mail@jaroslavtyc.com>
2019-06-11 07:53:18 +0200

    Some edge cases covered by tests

--- DrdPlus/Tests/Tables/Theurgist/Demons/DemonParameters/DemonRadiusTest.php
+++ DrdPlus/Tests/Tables/Theurgist/Demons/DemonParameters/DemonRadiusTest.php
@@ -2,6 +2,7 @@
 
 namespace DrdPlus\Tests\Tables\Theurgist\Demons\DemonParameters;
 
+use DrdPlus\Tables\Measurements\Distance\DistanceBonus;
 use DrdPlus\Tables\Tables;
 use DrdPlus\Tables\Theurgist\Demons\DemonParameters\DemonRadius;
 use DrdPlus\Tests\Tables\Theurgist\Spells\SpellParameters\Partials\CastingParameterTest;
@@ -15,5 +16,14 @@ class DemonRadiusTest extends CastingParameterTest
     {
         $demonRadius = new DemonRadius([123, 0], Tables::getIt());
         self::assertSame(123, $demonRadius->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_get_radius_as_distance()
+    {
+        $demonRadius = new DemonRadius([123, 0], Tables::getIt());
+        self::assertEquals(new DistanceBonus(123, Tables::getIt()->getDistanceTable()), $demonRadius->getDistanceBonus());
     }
 }
